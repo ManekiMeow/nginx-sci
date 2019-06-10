b530647026c10bcde38474576b9fd17ca3b67e11
Nagi <kiddopaul13@gmail.com>
2019-06-10 20:48:13 +0200

    Add scripts for (un)/install

---
+++ _Install.bat
@@ -0,0 +1,10 @@
+cd /d %~dp0
+ren C:\\Windows\\System32\\drivers\\etc\\hosts hosts.suzume
+copy hosts C:\\Windows\\System32\\drivers\\etc\\hosts
+certutil -enterprise -f -v -AddStore "Root" ./conf/certs/amzjp-master.cer
+certutil -enterprise -f -v -AddStore "Root" ./conf/certs/imgur-master.cer
+certutil -enterprise -f -v -AddStore "Root" ./conf/certs/ins-master.cer
+certutil -enterprise -f -v -AddStore "Root" ./conf/certs/pixiv.net-master.cer
+certutil -enterprise -f -v -AddStore "Root" ./conf/certs/reddit-master.cer
+certutil -enterprise -f -v -AddStore "Root" ./conf/certs/wikipedia-master.cer
+copy nginx-sci.lnk "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
---
+++ _Uninstall.bat
@@ -0,0 +1,2 @@
+copy C:\\Windows\\System32\\drivers\\etc\\hosts.suzume C:\\Windows\\System32\\drivers\\etc\\hosts
+del "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\nginx-sci.lnk"
